86bbb92345fc4100b7b3cf4992256b5a7fad67e2
jeff-eng <me@jeffeng.com>
2023-09-18 21:07:20 -0700

    Corrected typos on the menu item objects.

--- js/data.js
+++ js/data.js
@@ -1,23 +1,23 @@
 const menuArray = [
     {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushroom", "mozzarella"],
         price: 14,
         emoji: "🍕"
     },
     {
+        id: 1,
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
     },
         {
+        id: 2,
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
     }
 ];
